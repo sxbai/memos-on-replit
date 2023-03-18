3b1f934e6f9c905ba5b6dc89a4975e4d8ed1327c
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-18 19:58:24 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+echo "Memos一键更新脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/memos-on-replit"
+rm -rf memos && wget -O memos https://down2.sxbai.repl.co/memos/v0.11.2/memos
+echo "更新至v0.11.2版本，更新完毕！"
+echo "点击Run按钮重新启动本项目！！！"
