a08be7f61691fc3dfecf02007a73c6f19a2cd2de
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-18 19:00:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ wget -O memos https://down2.sxbai.repl.co/memos/v0.11.2/memos
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
-rm -rf build/
+rm -rf build/ && rm -rf README.md
