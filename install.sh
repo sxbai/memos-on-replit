5e60dc8066313e13a210ec498cf14c6eb1dacdce
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-18 18:58:16 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "Memos一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/memos-on-replit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/memos-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/memos-on-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/memos-on-replit/raw/master/main.sh
+wget -O memos https://down2.sxbai.repl.co/memos/v0.11.2/memos
+cp -r build/.replit . && cp -r build/replit.nix .
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+rm -rf build/
