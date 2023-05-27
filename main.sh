7a5161f6211676ec30412b465959b6ef1df8f847
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-05-27 14:41:21 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# 防休眠命令
+while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
+# 启动Memos
 echo "run memos >>> $(date '+%Y-%m-%d %H:%M:%S')"
 
 chmod +x ./memos
